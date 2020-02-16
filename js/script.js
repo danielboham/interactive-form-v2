492baec29e030245dab9f85fbc3ee172f45471a6
Daniel Boham <hello@danielboham.com>
2020-02-16 18:08:25 +0100

    add funct conditional display input field Job Role

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 
 const focusLoad = document.getElementById("name");
-
+const otherTitle = document.getElementById("other-title");
+const select = document.getElementById("title");
+const options = select.selectedOptions;
 
 //Focus field "Name" on page load
 window.onload = function() {
     focusLoad.focus();
 };
+
+//Initial hide Your Job Role input field
+otherTitle.style.display = 'none';
+
+select.addEventListener('change', function() {
+    for (i=0; i< options.length; i++) {
+    if (options[i].text == "Other") {
+        otherTitle.style.display = 'block';
+    } else {
+        otherTitle.style.display = 'none';
+        }
+    }
+});
