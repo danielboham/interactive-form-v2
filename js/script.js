a8d8fc6b66d08b5f912c4fc2fba7044862f7af66
Daniel Boham <hello@danielboham.com>
2020-04-02 15:52:16 +0200

    Refactor color options

--- js/script.js
+++ js/script.js
@@ -4,14 +4,29 @@ const otherTitle = document.getElementById("other-title");
 const select = document.getElementById("title");
 const options = select.selectedOptions;
 
-//Focus field "Name" on page load
+const design = document.getElementById("design");
+const designTheme = design.selectedOptions;
+const color = document.getElementById("color");
+const colorOptions = color.options;
+
+const themeSelect = document.getElementById('design');
+const themeOptions = themeSelect.selectedOptions;
+
+
+
+//FOCUS
+
+// Focus field "Name" on page load
 window.onload = function() {
     focusLoad.focus();
 };
 
-//Initial hide Your Job Role input field
+//JOB ROLE
+
+// Initial hide Your Job Role input field
 otherTitle.style.display = 'none';
 
+// EventListener for when select Other
 select.addEventListener('change', function() {
     for (i=0; i< options.length; i++) {
     if (options[i].text == "Other") {
@@ -21,3 +36,75 @@ select.addEventListener('change', function() {
         }
     }
 });
+
+
+// T-SHIRT
+
+// Create option element with instruction message + plus select this option
+var opt = document.createElement('option');
+opt.appendChild(document.createTextNode("Please select a T-shirt theme") );
+opt.value = "selectTheme";
+opt.setAttribute("id", "select-theme");
+opt.setAttribute("selected", "");
+color.appendChild(opt);
+
+// Give options initial id: invisible
+for (i=0; i<colorOptions.length; i++){
+    color[i].setAttribute("id", "invisible");
+    if (colorOptions[i].id === "invisible") {
+        colorOptions[i].style.display = 'none';
+    }
+}
+
+themeSelect.addEventListener('change', function() {
+    if (themeSelect[themeSelect.selectedIndex].value === 'js puns') {
+        for (i=0; i<colorOptions.length; i++) {
+            if (/\bPuns\b/.test(colorOptions[i].text) === true) {
+                colorOptions[i].setAttribute("id", "visible");
+                colorOptions[0].setAttribute("selected", "selected");
+            }
+
+            if (colorOptions[i].id === "visible") {
+                colorOptions[i].style.display = 'block';
+            }
+
+            if (/\bPuns\b/.test(colorOptions[i].text) === false) {
+                colorOptions[i].setAttribute("id", "invisible");
+            }
+
+            if (colorOptions[i].id === "invisible") {
+                colorOptions[i].style.display = 'none';
+            }
+
+        }
+    }
+
+    if (themeSelect[themeSelect.selectedIndex].value === 'heart js') {
+        for (i=0; i<colorOptions.length; i++) {
+
+            if (/\bI\b/.test(colorOptions[i].text) === true) {
+                colorOptions[i].setAttribute("id", "visible");
+                colorOptions[3].setAttribute("selected", "selected");
+            }
+
+            if (colorOptions[i].id === "visible") {
+                colorOptions[i].style.display = 'block';
+            }
+
+            if (/\bI\b/.test(colorOptions[i].text) === false) {
+                colorOptions[i].setAttribute("id", "invisible");
+            }
+
+            if (colorOptions[i].id === "invisible") {
+                colorOptions[i].style.display = 'none';
+            }
+
+        }
+    }
+
+});
+
+// nu update field first avaialble color
+
+
+
